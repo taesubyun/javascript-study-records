67b4a0f5e87e10d6cb0fa4e2b5e85ecbe6ab51cb
Taesub Yun <taesubyun@icloud.com>
2022-01-14 16:32:40 +0900

    about : 함수표현식과 선언식의 hoisting 차이

--- Content/Basic/callback.js
+++ Content/Basic/callback.js
@@ -19,3 +19,21 @@ const dacne = () => {
 };
 
 checkMood('sad', sing, cry);
+
+//
+
+console.log(helloB());
+//함수표현식
+let helloA = function () {
+    return '안녕하세요';
+};
+//함수선언식
+function helloB() {
+    return '안녕하세요 여러분##';
+}
+// 1. 함수선언식은 프로그램 실행 전에 코드 최상단으로 끌어 올려진다 -> hoisting
+// 2. 함수표현식은 hoisting이 되지 않는다
+//     해당 함수가 선언되기 전에는 접근이 불가능
+//     즉, helloA()라는 함수를 호출하고 싶다면 선언 이후에 호출해야 한다
+//      ex) console.log(helloA())
+//          let helloA = function () { }... -> X
